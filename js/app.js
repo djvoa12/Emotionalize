7d7a42219b0dfbae2e86dff2c67010b4822fc8f2
Thomas Lee <djvoa12@gmail.com>
2015-01-26 23:42:34 -0600

    redefined routes

--- js/app.js
+++ js/app.js
@@ -1,14 +1,6 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  this.route("tourdates");
-  this.resource("genres", function() {
-    this.resource("genre", { path: "/:genre_id"});
-  });
-});
-
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+  this.route("discover");
+  this.route("feelit");
 });
